2a5f5b6d0eee10f27f2cc2a9cf0594b0b2a4271d
Julien <julienmulle@gmail.com>
2021-10-18 14:08:24 +0200

    creation de l'app + screen RecipesLiist

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RecipesList from './src/screen/RecipesList';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <RecipesList />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -12,10 +14,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
