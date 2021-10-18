cbc6f46f3ae592da229eed5119a517ee60bb2239
Julien <julienmulle@gmail.com>
2021-10-18 14:38:08 +0200

    maj

--- src/api/recipes/useFetchRecipes.js
+++ src/api/recipes/useFetchRecipes.js
@@ -1,13 +1,18 @@
 import axios from "axios"
-// const {getAllRecipes} = useFetchRecipes()
+
 const URL_API = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "c0ef4429bd084f21b5f1c36dbe2eb701";
+const MAX_RESULT = 30
+
 export const useFetchRecipes = () => {
 
     const getAllRecipes = async() => {
         try {
         const response = await axios.get(URL_API, {
-            apiKey: API_KEY,
+            params:{
+                apiKey: API_KEY,
+            number: MAX_RESULT
+            }
         })
         console.log("response", response.data.results)
         } catch(e) {
--- src/screen/RecipesList/index.js
+++ src/screen/RecipesList/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Text } from 'react-native';
+import { useFetchRecipes } from '../../api/recipes/useFetchRecipes';
 
 export default function RecipesList() {
+    const { getAllRecipes } = useFetchRecipes()
+
+    //affichage de l'ecran
+    useEffect(() => {
+        getAllRecipes()
+    }, [])
     return (
         <>
             <Text>RecipesList</Text>
