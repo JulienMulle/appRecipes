3df01e00912a5ea6a8bb5879bbac60585c341c81
Julien <julienmulle@gmail.com>
2021-10-19 10:54:13 +0200

    requête axios pour les détails des recettes

--- src/api/recipes/useFetchRecipes.js
+++ src/api/recipes/useFetchRecipes.js
@@ -2,17 +2,19 @@ import axios from "axios"
 import { useDispatch } from "react-redux";
 import { addRecipes } from "../../redux/actions";
 
-const URL_API = "https://api.spoonacular.com/recipes/complexSearch";
+
+const URL_API = "https://api.spoonacular.com/recipes";
 const API_KEY = "c0ef4429bd084f21b5f1c36dbe2eb701";
 //const {REACT_APP_KEY} = process.env
 const MAX_RESULT = 15
 
-export const useFetchRecipes = () => {
+export const useFetchRecipes = (id) => {
     const dispatch = useDispatch()
 
+    // requete pour acceder à toutes les recettes
     const getAllRecipes = async() => {
         try {
-        const response = await axios.get(URL_API, {
+        const response = await axios.get(`${URL_API}/complexSearch`, {
             params:{
                 apiKey: API_KEY,
             number: MAX_RESULT
@@ -21,8 +23,21 @@ export const useFetchRecipes = () => {
         
         dispatch(addRecipes(response.data.results))
         } catch(e) {
-            
-            
+                 
+        }
+    }
+
+    //requete pour acceder aux details des recettes
+    const getRecipesById = async() => {
+        try {
+            const response = await axios.get(`${URL_API}/${id}/information`, {
+                params:{
+                    apiKey: API_KEY,
+                number: MAX_RESULT
+                }
+            })
+        } catch(e) {
+            console.error()
         }
     }
 
