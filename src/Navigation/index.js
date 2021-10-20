5a60ebc2733dbb29d34b00f1564df5633c718902
Julien <julienmulle@gmail.com>
2021-10-20 11:04:40 +0200

    creation de la navigation par Tabs

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //components
 import RecipesList from '../screen/RecipesList';
@@ -8,14 +9,21 @@ import RecipesDetails from '../screen/RecipesDetails';
 import TaskScreen from '../screen/tasks';
 
 export default function AppNavigation() {
+
     const Stack = createNativeStackNavigator();
+    const Tabs = createBottomTabNavigator();
+
     return (
         <NavigationContainer>
-        <Stack.Navigator>
+          <Tabs.Navigator>
+            <Tabs.Screen name="Recettes" component={RecipesList} />
+            <Tabs.Screen name="Liste" component={TaskScreen} />
+          </Tabs.Navigator>
+        {/*<Stack.Navigator>
           <Stack.Screen name="TaskList" component={TaskScreen} />
           <Stack.Screen name="RecipesList" component={RecipesList} />
           <Stack.Screen name="RecipesDetails" component={RecipesDetails} />
-        </Stack.Navigator>
+        </Stack.Navigator>*/}
       </NavigationContainer>
     )
 }
